9faceb1ef75b46efbb1d76655a40fe6582b7b466
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2024-11-05 13:55:48 +0700

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import daStyle from 'eslint-config-dicodingacademy';
+import globals from "globals";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  daStyle,
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: globals.browser }},
 ];
